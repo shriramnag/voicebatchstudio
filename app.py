a987f1847924920034a873b8bb958f7c3ee608f9
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-01-03 09:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import os
+import sys
 
+# फोल्डर पाथ सेट करना (ताकि कोई एरर न आए)
+base_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(base_path, 'src'))
+
+# अब यहाँ अपना Chatterbox वाला कोड पेस्ट करें...
